2ad773f62f2a613896ca775cc6050d1ea845e8db
blackmouse572 <ngocvlqt1995@gmail.com>
2024-11-23 18:11:00 +0700

    add error log

--- src/routes.ts
+++ src/routes.ts
@@ -22,42 +22,46 @@ router.addDefaultHandler(async ({ enqueueLinks, page, log }) => {
 router.addHandler('detail', async ({ request, page, log }) => {
     const title = await page.title();
     log.info(`Get details: ${title}`, { url: request.loadedUrl });
-    const labelNode = await page.$('h1.title-detail');
-    const descriptionNode = await page.$('p.description');
-    const detailNode = await page.$('article.fck_detail');
+    try {
+        const labelNode = await page.$('h1.title-detail');
+        const descriptionNode = await page.$('p.description');
+        const detailNode = await page.$('article.fck_detail');
 
-    const label = await labelNode?.evaluate((e) => e.innerText);
-    const description = await descriptionNode?.evaluate((e) => e.innerText);
-    const detail = await detailNode
-        ?.evaluate((e) => e.innerHTML)
-        .then((html) => {
-            return html.replace(/<[^>]*>?/gm, '');
+        const label = await labelNode?.evaluate((e) => e.innerText);
+        const description = await descriptionNode?.evaluate((e) => e.innerText);
+        const detail = await detailNode
+            ?.evaluate((e) => e.innerHTML)
+            .then((html) => {
+                return html.replace(/<[^>]*>?/gm, '');
+            });
+        await Dataset.pushData({
+            title,
+            label,
+            description: [description, detail].join('\n'),
+            url: request.loadedUrl,
         });
-    await Dataset.pushData({
-        title,
-        label,
-        description: [description, detail].join('\n'),
-        url: request.loadedUrl,
-    });
-    // const dataset = new NewsDocument({
-    //     title,
-    //     label,
-    //     description: [description, detail].join('\n'),
-    //     url: request.loadedUrl,
-    // });
+        // const dataset = new NewsDocument({
+        //     title,
+        //     label,
+        //     description: [description, detail].join('\n'),
+        //     url: request.loadedUrl,
+        // });
 
-    // await dataset
-    //     .save({})
-    //     .then((e) => {
-    //         if (!e.errors) {
-    //             log.info(`Saved: ${title}`, { url: request.loadedUrl });
-    //         }
-    //     })
-    //     .catch((err) => {
-    //         // if the URL is already saved, we can ignore the error
-    //         if (err.code !== 11000) {
-    //             throw err;
-    //         }
-    //         log.info(`URL already saved: ${title}`);
-    //     });
+        // await dataset
+        //     .save({})
+        //     .then((e) => {
+        //         if (!e.errors) {
+        //             log.info(`Saved: ${title}`, { url: request.loadedUrl });
+        //         }
+        //     })
+        //     .catch((err) => {
+        //         // if the URL is already saved, we can ignore the error
+        //         if (err.code !== 11000) {
+        //             throw err;
+        //         }
+        //         log.info(`URL already saved: ${title}`);
+        //     });
+    } catch (error) {
+        log.error('Error:', error as Error);
+    }
 });
